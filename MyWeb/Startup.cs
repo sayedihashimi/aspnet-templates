b5d07fb8e6f4f2d319ad1e2ed7b4cb804aad1b79
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-09-23 17:18:36 -0400

    adding files

--- MyWeb/Startup.cs
+++ MyWeb/Startup.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyWeb.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +23,12 @@ namespace MyWeb {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
 
@@ -26,6 +36,7 @@ namespace MyWeb {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else {
                 app.UseExceptionHandler("/Error");
@@ -38,6 +49,7 @@ namespace MyWeb {
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => {
