41592dd9ff8147b97ec95f98d0862b1b60040df0
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-09-29 11:18:54 -0400

    adding files

--- MyBlazor/Program.cs
+++ MyBlazor/Program.cs
@@ -16,6 +16,10 @@ namespace MyBlazor {
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddMsalAuthentication(options => {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+            });
+
             await builder.Build().RunAsync();
         }
     }
